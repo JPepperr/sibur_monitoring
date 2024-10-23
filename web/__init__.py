eaab763d5724233cecbb2cfb2241e753c00efb0e
prosto <danyazazackiy@gmail.com>
2024-10-23 16:52:59 +0500

    html add

--- web/__init__.py
+++ web/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from database.models import Problems
 import datetime
 
@@ -27,17 +27,39 @@ async def submit():
     return render_template('success.html')
 
 
-@app.route('/show')
+@app.route('/show', methods=['GET'])
 async def show():
-    problems = await Problems.all().values()
+    status = request.args.get('status', 'START')
+
+    if status == 'START':
+        problems = await Problems.filter(status='START').values()
+    elif status == 'IN_PROGRESS':
+        problems = await Problems.filter(status='IN_PROGRESS').values()
+    elif status == 'END':
+        problems = await Problems.filter(status='END').values()
+
     data = []
     for v in problems:
-        data.append([v['description'], v['message'], v['time']])
-        print(v['time'])
+        data.append([v['description'], v['message'], v['time'], v['id']])
 
     data.sort(key=lambda x: x[2], reverse=True)
 
-    return render_template('show.html', data=data)
+    return render_template('show.html', data=data, status=status)
+
+
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+async def edit(id):
+    problem = await Problems.get(id=id)
+    if request.method == 'POST':
+        problem.priority = request.form.get('priority')
+        problem.description = request.form.get('description')
+        problem.message = request.form.get('message')
+        await problem.save()
+
+        return redirect('/show')
+
+    return render_template('edit.html', problem=problem)
 
 
 def setup():
