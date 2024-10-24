07a95835282c0347156aebf2763cb01c59ae172e
prosto <danyazazackiy@gmail.com>
2024-10-24 12:10:58 +0500

    rebase

--- web/__init__.py
+++ web/__init__.py
@@ -27,25 +27,37 @@ async def submit():
     return render_template('success.html')
 
 
-@app.route('/show', methods=['GET'])
+@app.route('/show')
 async def show():
-    status = request.args.get('status', 'START')
+    tab = request.args.get('tab', 'start')  # Получаем вкладку, по умолчанию 'start'
 
-    if status == 'START':
+    start_data = []
+    in_progress_data = []
+    end_data = []
+
+    if tab == 'start':
         problems = await Problems.filter(status='START').values()
-    elif status == 'IN_PROGRESS':
+        for v in problems:
+            start_data.append([v['description'], v['message'], v['time'], v['id']])
+        start_data.sort(key=lambda x: x[2], reverse=True)
+
+    elif tab == 'in_progress':
         problems = await Problems.filter(status='IN_PROGRESS').values()
-    elif status == 'END':
+        for v in problems:
+            in_progress_data.append([v['description'], v['message'], v['time'], v['id']])
+        in_progress_data.sort(key=lambda x: x[2], reverse=True)
+
+    elif tab == 'end':
         problems = await Problems.filter(status='END').values()
+        for v in problems:
+            end_data.append([v['description'], v['message'], v['time'], v['id']])
+        end_data.sort(key=lambda x: x[2], reverse=True)
 
-    data = []
-    for v in problems:
-        data.append([v['description'], v['message'], v['time'], v['id']])
-
-    data.sort(key=lambda x: x[2], reverse=True)
-
-    return render_template('show.html', data=data, status=status)
-
+    return render_template('show.html',
+                           start_data=start_data,
+                           in_progress_data=in_progress_data,
+                           end_data=end_data,
+                           tab=tab)
 
 
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
@@ -62,5 +74,23 @@ async def edit(id):
     return render_template('edit.html', problem=problem)
 
 
+@app.route('/take/<int:problem_id>', methods=['POST'])
+async def take_problem(problem_id):
+    problem = await Problems.get(id=problem_id)
+    if problem:
+        problem.status = 'IN_PROGRESS'
+        await problem.save()
+    return redirect('/show?tab=start')  # Возвращаемся на вкладку "Ждут действий"
+
+
+@app.route('/solve/<int:problem_id>', methods=['POST'])
+async def solve_problem(problem_id):
+    problem = await Problems.get(id=problem_id)
+    if problem:
+        problem.status = 'END'  # Меняем статус задачи на "Решено"
+        await problem.save()
+    return redirect('/show?tab=in_progress')  # Возвращаемся на вкладку "В процессе"
+
+
 def setup():
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
