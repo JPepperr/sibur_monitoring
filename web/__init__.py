25002c841280f1959b7090e8d76ad8b694a1366d
Your Name <you@example.com>
2024-10-24 14:46:58 +0300

    add resposible

--- web/__init__.py
+++ web/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-from database.models import Problems
+from database.models import Problems, Person
 import datetime
 
 app = Flask(__name__)
@@ -38,19 +38,23 @@ async def show():
     if tab == 'start':
         problems = await Problems.filter(status='START').values()
         for v in problems:
-            start_data.append([v['description'], v['message'], v['time'], v['id']])
+            start_data.append([v['description'], v['message'], v['time'], v['id'], 1])
+
         start_data.sort(key=lambda x: x[2], reverse=True)
 
     elif tab == 'in_progress':
-        problems = await Problems.filter(status='IN_PROGRESS').values()
+        problems = await Problems.filter(status='IN_PROGRESS').all()
         for v in problems:
-            in_progress_data.append([v['description'], v['message'], v['time'], v['id']])
+            in_progress_data.append([v.description, v.message, v.time, v.id, "<no>" if not v.responsible else (await v.responsible.first()).full_name])
+
         in_progress_data.sort(key=lambda x: x[2], reverse=True)
 
     elif tab == 'end':
-        problems = await Problems.filter(status='END').values()
+        problems = await Problems.filter(status='END').all()
+        
         for v in problems:
-            end_data.append([v['description'], v['message'], v['time'], v['id']])
+            end_data.append([v.description, v.message, v.time, v.id, "<no>" if not v.responsible else (await v.responsible.first()).full_name])
+
         end_data.sort(key=lambda x: x[2], reverse=True)
 
     return render_template('show.html',
